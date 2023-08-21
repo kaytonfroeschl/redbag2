2555896c14a0b4b48f8d30fd056c32fb86cfac1a
Kayton Froeschl <kaytonfroeschl@Kaytons-MacBook-Pro-2.local>
2023-08-21 15:51:12 -0400

    add

--- src/components/Sponsor/EditSponsorForm.js
+++ src/components/Sponsor/EditSponsorForm.js
@@ -1,5 +1,29 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import { gql, useMutation, useQuery } from '@apollo/client';
+import { updateSponsor } from '../../graphql/mutations';
+import { listSponsors, getSponsor } from '../../graphql/queries';
+import Dialog from '@mui/material/Dialog';
+import { DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
 
 export default function EditSponsorForm(){
+
+/* ==============================================================================================
+                                        Set Variables
+================================================================================================*/
+    const [form_id, setFormID] = useState('');
+    const [form_name, setFormName] = useState('');
+    const [form_address, setFormAddress] = useState('');
+    const [form_email, setFormEmail] = useState('');
+    const [form_inst, setFormInst] = useState('');
+    const [form_phone, setFormPhone] = useState('');
     
+        
 };
