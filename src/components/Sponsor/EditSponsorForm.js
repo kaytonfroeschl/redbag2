bb49af696c7a59910e8102e06bc24e00cda141d2
Bill Froeschl <bfroeschl@gmail.com>
2023-10-04 14:24:36 -0500

    Now validating and showing mutation errors

--- src/components/Sponsor/EditSponsorForm.js
+++ src/components/Sponsor/EditSponsorForm.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { gql, useMutation, useQuery } from '@apollo/client';
 import { updateSponsor } from '../../graphql/mutations';
 import { listSponsors } from '../../graphql/queries';
 import Dialog from '@mui/material/Dialog';
-import { DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { DialogActions, DialogContent, DialogTitle, Alert } from '@mui/material';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl';
@@ -17,71 +17,68 @@ export default function EditSponsorForm({ open, handleClose, sponsor }){
     const [form_street, setFormStreet] = useState(sponsor.AddressStreet);
     const [form_city, setFormCity] = useState(sponsor.AddressCity);
     const [form_state, setFormState] = useState(sponsor.AddressState);
-    const [form_zip, setFormZip] = useState(sponsor.Zip);
+    const [form_zip, setFormZip] = useState(sponsor.AddressZip);
     const [form_email, setFormEmail] = useState(sponsor.Email);
     const [form_inst, setFormInst] = useState(sponsor.Institution);
     const [form_phone, setFormPhone] = useState(sponsor.Phone);
     const [form_ya, setFormYA] = useState(sponsor.YearsActive);
+    const [generalError, setGeneralError] = useState('');
 
-    //Bill: pretty sure you don't want to do this.  Every time "sponsor" changes, you change "sponsor".
-    // useEffect(() => {
-    //     setFormID(sponsor.id);
-    //     setFormName(sponsor.FirstName);
-    //     setFormFirstName(sponsor.FirstName);
-    //     setFormLastName(sponsor.LastName);
-    //     setFormAddress(sponsor.Address);
-    //     setFormStreet(sponsor.AddressStreet);
-    //     setFormCity(sponsor.AddressCity);
-    //     setFormState(sponsor.AddressState);
-    //     setFormZip(sponsor.AddressZip);
-    //     setFormEmail(sponsor.Email);
-    //     setFormInst(sponsor.Institution);
-    //     setFormPhone(sponsor.Phone);
-    //     setFormYA(sponsor.YearsActive);
-    // }, [sponsor])
+    const resetValues = (sponsor) => {
+        setFormFirstName(sponsor.FirstName);
+        setFormLastName(sponsor.LastName);
+        setFormStreet(sponsor.AddressStreet);
+        setFormCity(sponsor.AddressCity);
+        setFormState(sponsor.AddressState);
+        setFormZip(sponsor.AddressZip);
+        setFormEmail(sponsor.Email);
+        setFormInst(sponsor.Institution);
+        setFormPhone(sponsor.Phone);
+        setFormYA(sponsor.YearsActive);
+    };
 
-/* ==============================================================================================
-                                        OnChange Handle Functions 
-================================================================================================*/
-    function handleFormFirstName(event){
-        setFormFirstName(event.target.value);
-    }
+    const updateSponsorInMemory = (newSponsorValues) => {
+        sponsor.FirstName = newSponsorValues.FirstName;
+        sponsor.LastName = newSponsorValues.LastName;
+        sponsor.AddressStreet = newSponsorValues.AddressStreet;
+        sponsor.AddressCity = newSponsorValues.AddressCity;
+        sponsor.AddressState = newSponsorValues.AddressState;
+        sponsor.AddressZip = newSponsorValues.AddressZip;
+        sponsor.Email = newSponsorValues.Email;
+        sponsor.Institution = newSponsorValues.Institution;
+        sponsor.Phone = newSponsorValues.Phone;
+        sponsor.YearsActive = newSponsorValues.YearsActive;
+    };
 
-    function handleFormLastName(event){
-        setFormLastName(event.target.value);
-    }
+    function handleFormFirstName(event) {setFormFirstName(event.target.value)}
+    function handleFormLastName(event)  {setFormLastName(event.target.value)}
+    function handleFormStreet(event)    {setFormStreet(event.target.value)}
+    function handleFormCity(event)      {setFormCity(event.target.value)}
+    function handleFormState(event)     {setFormState(event.target.value)}
+    function handleFormZip(event)       {setFormZip(event.target.value)}
+    function handleFormEmail(event)     {setFormEmail(event.target.value)}
+    function handleFormInst(event)      {setFormInst(event.target.value)}
+    function handleFormPhone(event)     {setFormPhone(event.target.value)}
+    function handleFormYA(event)        {setFormYA(event.target.value)}
 
-    function handleFormStreet(event){
-        setFormStreet(event.target.value);
-    }
+    const sponsorIsValid = (sponsor) => {
+        //Must have First AND Last or Institution
+        if (! sponsor) return;
+        if (sponsor.Institution.length < 1) {
+            const firstAndLast = sponsor.FirstName + sponsor.LastName;
+            
+            if (firstAndLast.length < 1) {
+                setGeneralError("You must specify a First AND Last Name OR an Institution Name");
+                return false;
+            };
 
-    function handleFormCity(event){
-        setFormCity(event.target.value);
-    }
-
-    function handleFormState(event){
-        setFormState(event.target.value);
-    }
-
-    function handleFormZip(event){
-        setFormZip(event.target.value);
-    }
-
-    function handleFormEmail(event){
-        setFormEmail(event.target.value);
-    }
-
-    function handleFormInst(event){
-        setFormInst(event.target.value);
-    }
-
-    function handleFormPhone(event){
-        setFormPhone(event.target.value);
-    }
-
-    function handleFormYA(event){
-        setFormYA(event.target.value);
-    }
+            if (sponsor.FirstName.length < 1 || sponsor.LastName < 1) {
+                setGeneralError("You must have a First AND Last Name");
+                return false;
+            };
+        };
+        return true;
+    };
 
 /* ==============================================================================================
                                         Apollo Call 
@@ -89,10 +86,31 @@ export default function EditSponsorForm({ open, handleClose, sponsor }){
     const [updateSponsorMutation] = useMutation(gql(updateSponsor));
     const {loading, error} = useQuery(gql(listSponsors));
     if(loading) {return <div>Loading...</div>}
-    if(error) {return <div>Error in useMutation(gql(updateSponsor)): {error}</div>}
+    if(error) {return <div>Error updating Sponsor: {error}</div>}
 
     async function handleUpdate(e) {
         e.preventDefault();
+        setGeneralError("");
+
+        let sponsorChanges = {
+            id: sponsor.id, 
+            FirstName: form_firstname, 
+            LastName: form_lastname, 
+            Phone: form_phone, 
+            Email: form_email, 
+            Address: '', 
+            AddressStreet: form_street, 
+            AddressCity: form_city, 
+            AddressState: form_state, 
+            AddressZip: form_zip, 
+            Institution: form_inst, 
+            YearsActive: form_ya,
+        };
+
+        if ( ! sponsorIsValid(sponsorChanges)) return;
+
+        //I tried to use "sponsorChanges" as the "input: " in the mutation below but it will not work
+        //Even though they are IDENTICAL (or were before I added the "whatIfNull" trying to make it work)
         try {
             await updateSponsorMutation({
                 variables: { input: { 
@@ -110,17 +128,33 @@ export default function EditSponsorForm({ open, handleClose, sponsor }){
                     YearsActive: form_ya}},
                 refetchQueries: [{ query: gql(listSponsors) }], // Refetch the query to update the list
             });
+            updateSponsorInMemory(sponsorChanges);
             handleClose();
         } catch (error) {
-            console.error("Mutation error: ", error);
+            setGeneralError("Update Sponsor threw an error: " + error)
         }
+    };
+
+    const handleCancel = () => {
+        setGeneralError("");
+        resetValues(sponsor);  //resets all values to the way they were on open
+        handleClose();
     }
+
+    const showErrorNotification = () => {
+        let result = <></>;
+        if (generalError) {
+            result = <Alert severity="error">{generalError}</Alert>
+        };
+        return result;
+    };
 
     return(
         <React.Fragment>
         <Dialog open={open} onClose={handleClose}>
         <DialogTitle style={{fontWeight:'bold'}}>Edit</DialogTitle>
             <DialogContent>
+                {showErrorNotification()}
                 <FormControl
                     required={true}
                     variant="outlined"
@@ -200,7 +234,7 @@ export default function EditSponsorForm({ open, handleClose, sponsor }){
                         onChange={handleFormState}
                     />
                     <TextField
-                        label="Zipcode"
+                        label="Zip Code"
                         value={form_zip !== null ? form_zip : ""}
                         onChange={handleFormZip}
                     />
@@ -220,9 +254,10 @@ export default function EditSponsorForm({ open, handleClose, sponsor }){
                 </Box>
                 </Box>
                 </FormControl>
+                {showErrorNotification()}
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleCancel}>Cancel</Button>
                 <Button onClick={handleUpdate}>Update</Button>
             </DialogActions>
     </Dialog>
