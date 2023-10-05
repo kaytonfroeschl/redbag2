250ece13fd1641e6d606b3a691ec40c332e0f255
Kayton Froeschl <kaytonfroeschl@Kaytons-MacBook-Pro-2.local>
2023-10-05 13:14:51 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { useEffect } from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import ChildrenScreen from './screens/ChildrenScreen';
 import HeaderBar from './components/HeaderBar';
 import NavMenu from './components/NavMenu';
+import { Auth } from 'aws-amplify';
 
-function App() {
+function App({ user }) {
   
   return (
-    <div>
-      <HeaderBar />
-      <NavMenu />
-    </div>
+    <Authenticator hideSignUp={true}>
+      {
+        (
+          { signOut, user }
+        ) => (
+          <main>
+            <HeaderBar />
+            <NavMenu />           
+          </main>
+        )
+      }
+    </Authenticator>
+
   );
 }
 
